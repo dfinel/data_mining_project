4a30273fc5bd4dc0e5917744fae4cdd342e34277
dfinel <ledan.finel@gmail.com>
2023-12-07 17:09:31 +0200

    Changed the results into a list of dictionary containing attributes of each objects and added some info to the README

--- milestone1.py
+++ milestone1.py
@@ -1,4 +1,5 @@
 import grequests
+import sys
 from bs4 import BeautifulSoup
 from import_json import format_url
 from import_json import upper_range_nbr_product
@@ -8,16 +9,17 @@ from import_json import url_website
 from import_json import good_response
 
 
-
 def get_all_urls():
     """ Returns urls of each page of padel rackets from the website """
     url = format_url
     return [url.format(i) for i in range(0, upper_range_nbr_product, nbr_product_per_page)]
 
-
 def get_link_of_products_page(response):
     """ returns the links of each product page from a specific page"""
-    soup = BeautifulSoup(response.content, 'html.parser')
+    try:
+        soup = BeautifulSoup(response.content, 'html.parser')
+    except AttributeError:
+        raise AttributeError("The url given is not working. Please check it.")
     product_links = [url_website + link['href'] for link in soup.find_all('a', class_="link")]
     return product_links
 
@@ -43,8 +45,12 @@ def send_requests_on_products():
 
 
 def get_all_infos():
-    """ returns all the info we need about the product : name, price, saving, and the reference of the product"""
-    my_requests = send_requests_on_products()
+    """ returns all the info we need about the product """
+    try:
+        my_requests = send_requests_on_products()
+    except AttributeError:
+        print("The given url is not working. Please check it.")
+        sys.exit()
     info = []
     for various_request in my_requests:
         for request in grequests.imap(various_request, size=number_of_threads):
@@ -52,36 +58,37 @@ def get_all_infos():
             if response.status_code != good_response:
                 raise FileExistsError(f'Status code is not 200 but {response.status_code}')
             soup = BeautifulSoup(response.content, 'html.parser')
-            product_number = soup.find_all("span", class_="attr-value product-id")
-            name = soup.find_all("span", class_="product-name-sub", itemprop="name")
-            price = soup.find_all("span", class_="value", itemprop="price")
-            saving = soup.find_all("span", class_="saving")
-            all_info = [(each_name.text.strip(), each_price.text.strip(), each_saving.text.strip(),
-                         each_product_details.text.strip()) for
-                        each_name, each_price, each_saving, each_product_details in
-                        zip(name, price, saving, product_number)]
-            for infor in all_info:
-                print(infor)
+            attributes = {}
+
+            # Extracting product details
+            attributes["name"] = soup.find("span", class_="product-name-sub", itemprop="name").text.strip()
+            attributes["price"] = soup.find("span", class_="value", itemprop="price").text.strip()
+
+            try:
+                attributes["saving"] = soup.find("span", class_="saving").text.strip()
+            except AttributeError:
+                attributes["saving"] = str(0)
+
+            # Extracting product attributes
+            for title, value in zip(soup.find_all("span", class_="attr-title"),
+                                    soup.find_all("span", class_="attr-value")):
+                key = title.text.strip()
+                attributes[key] = value.text.strip()
+
+            if soup.find("i", class_="icon icon-attention"):
+                attributes["in stock"] = "No"
+            else:
+                attributes["in stock"] = "Yes"
+
+            all_info = [attributes]
+            info.extend(all_info)
+            print(info)
     return info
-get_all_infos()
-#
-#
-# def send_requests():
-#     """ returns the requests and set the header to avoid code repetition"""
-#     # headers = {'User-Agent': 'Your User Agent', }
-#     links = get_all_urls()
-#     my_requests = [grequests.get(link) for link in links]  # creates the request
-#     return my_requests
-#
-#
-# def get_price_and_name():
-#     requests = send_requests()
-#     prices_and_names = []
-#     for request in grequests.imap(requests, size=10):
-#         response = request
-#         soup = BeautifulSoup(response.content, 'html.parser')
-#         price = soup.find_all('span', itemprop="price")
-#         name = soup.find_all("a", class_="link", itemprop="url")
-#         name_and_price = [(each_name.text.strip(),each_price.text.strip()) for each_name,each_price in zip(name,price)]
-#         prices_and_names.append(name_and_price)
-#     return prices_and_names
+
+
+def main():
+   get_all_infos()
+
+
+if __name__ == '__main__':
+    main()
